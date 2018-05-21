a67d549441e5daa716891870356cfcbc437dd76e
caho1212 <caho1212@edu.skovde.se>
2018-05-21 18:30:09 +0200

    mid work transfer

---
+++ TheDeserter/CollisionTileMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TheDeserter
+{
+    class CollisionTileMap : TileMap
+    {
+        public CollisionTileMap(XElement layerData) : base(layerData)
+        {
+
+        }
+    }
+}
--- TheDeserter/TileMap.cs
+++ TheDeserter/TileMap.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TheDeserter
 {
     class TileMap
     {
 
-        public Tile[,] tileMap;
+        private Tile[,] _layerTileMap;
+        private XContainer layerData;
+        private string _name;
 
+        public Tile[,] LayerTileMap
+        {
+            get { return _layerTileMap; }
+            set { _layerTileMap = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public TileMap(XElement layerData)
+        {
+            this.layerData = layerData;
+            Name = layerData.Attribute("name").Value;
+        }
 
         public void CheckCollisions ()
         {
-
+            
         }
 
     }
